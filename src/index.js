644c33617cba69e226039f83211a2433239ceaf8
bdistin <bdistin@gmail.com>
2017-08-17 09:52:18 -0500

    test again

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 for (const [key, method] of Object.entries(redis.RedisClient.prototype)) {
-	if (typeof method === 'function') redis.RedisClient.prototype[key] = promisify(method);
+	if (typeof method === 'function') redis.RedisClient.prototype[`${key}Async`] = promisify(method);
 }
 
 for (const [key, method] of Object.entries(redis.Multi.prototype)) {
-	if (typeof method === 'function') redis.Multi.prototype[key] = promisify(method);
+	if (typeof method === 'function') redis.Multi.prototype[`${key}Async`] = promisify(method);
 }
 
 module.exports = redis;
