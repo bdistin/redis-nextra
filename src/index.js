68f7654adedc1c09923c509bf1ca95ed773345d2
bdistin <bdistin@gmail.com>
2017-08-17 09:49:11 -0500

    nope, this was the problem

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 for (const [key, method] of Object.entries(redis.RedisClient.prototype)) {
-	if (typeof method === 'function') redis.RedisClient[`${key}Async`] = promisify(method);
+	if (typeof method === 'function') redis.RedisClient.prototype[key] = promisify(method);
 }
 
 for (const [key, method] of Object.entries(redis.Multi.prototype)) {
-	if (typeof method === 'function') redis.Multi[`${key}Async`] = promisify(method);
+	if (typeof method === 'function') redis.Multi.prototype[key] = promisify(method);
 }
 
 module.exports = redis;
